92df3e9cc26b24347ff521050f99b07c560e90f3
Denise Erazmus <denise.erazmus@gmail.com>
2017-10-06 22:47:01 -0500

    created basic HTML, started pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+//start timer and game when "I soloemly swear that I am up to no good" button is clicked and change to quiz page ( switch done in html)
+
+// display the remaining time 
+
+// create question
+// create answers for each question ( use radio buttons)( can only select one answer)
+//variables needed
+
+//var startScreen;
+//var gameHTML;
+var counter = 30;
+var questionArray = 
+
+//create submit button
+
+//if time runs out move to "summary" screen
+
+// after submit it select, move to "summary" screen
+
+//display on screen number of answers correct and number incorrect
+
+//upon hitting mischief managed, reset clock
+};
